28a68fa46566115802ea5e5292d616057299bf57
Vivian Phung <dev+github@vivianphung.com>
2023-08-06 18:39:32 -0400

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   );
 }
