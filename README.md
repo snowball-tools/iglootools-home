a59244a4ee49af9b86892215fdd1a96c8678f3ea
Vivian Phung <dev+github@vivianphung.com>
2023-08-06 20:45:21 -0400

    associated-domains

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ yarn install
 ```
 yarn dev
 ```
+
+## [Apple App Association](https://developer.apple.com/documentation/xcode/supporting-associated-domains) Start
+
+test via
+
+```bash
+curl -v https://app-site-association.cdn-apple.com/a/v1/<url>
+```
--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/.well-known/apple-app-site-association",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/json",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
