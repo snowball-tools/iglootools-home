7f0da51c5ea9c387ef335a2f24439773fc5b0859
Vivian Phung <dev+github@vivianphung.com>
2023-08-10 15:04:42 -0400

    webhook

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -173,7 +173,7 @@ export default function Login() {
               type="submit"
               className="mt-4 w-full px-4 py-2 bg-gray-500 text-white font-bold rounded transition-colors duration-200 hover:bg-gray-400 disabled:opacity-20"
               onClick={sendUserOperation}
-              disabled={false}
+              disabled={true}
             >
               [WIP] Send User Operation
             </button>
