01f89fb8ebef5a601f9854f8940ad600e742686b
Vivian Phung <dev+github@vivianphung.com>
2023-08-10 14:50:18 -0400

    user op webhook

---
+++ src/pages/api/iglooNFTWebhook.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  res.status(200).json({ success: true });
+}
