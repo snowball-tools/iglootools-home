eca174abd4062e1343cae995c07f69808fc56fbd
Vivian Phung <dev+github@vivianphung.com>
2023-08-21 17:02:56 -0400

    height of animation

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -57,7 +57,7 @@ const LoadingAnimation = ({
   }, []);
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <svg
         width="100%"
         height="100%"
