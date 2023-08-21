3d7029b70111a719d815ade2596a15b1e3b583fe
Vivian Phung <dev+github@vivianphung.com>
2023-08-20 21:12:05 -0400

    remove test view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import PasskeyMainView from "./PasskeyMainView";
-// import { NEXT_PUBLIC_DEBUG } from "@/helpers/env";
-import TestView from "@/components/TestView";
-
-const NEXT_PUBLIC_DEBUG = true;
 
 const Home = () => {
-  return <>{NEXT_PUBLIC_DEBUG ? <TestView /> : <PasskeyMainView />}</>;
+  return <PasskeyMainView />;
 };
 
 export default Home;
